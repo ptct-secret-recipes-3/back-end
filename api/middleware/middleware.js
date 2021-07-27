83b7864541a592385cb8417d8f8ac5eb1461bb65
Molly Brown <mollybrown0516@gmail.com>
2021-07-27 12:59:35 -0400

    Completed building the migration tables

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,5 @@
 const users = require("../auth/auth-model.js");
-const bcrypt = require("bcryptjs");
+//const bcrypt = require("bcryptjs");
 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~LOGIN/REGISTER MIDDLEWARE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -72,27 +72,49 @@ const checkUsernameExists = async (req, res, next) => {
 
 const checkRecipePayload = (req, res, next) => {
     console.log("in the checkPayload")
-      const username = req.body.username;
-      const password = req.body.password;
-      if (!username || !password) {
-          return res.status(400).json(
-              "username and password required"
-          )
+      const newRecipe = req.body;
+      if (!newRecipe.title || !newRecipe.source || !newRecipe.ingredients || !newRecipe.instructions) {
+          return res.status(400).json({ message:
+              "All fields are required"
+            })
+      }else if (typeof newRecipe.title !== 'string' ) {
+          return res.status(400).json({message: "title of recipe must be a string"})
+      } else if ( newRecipe.title.length < 3 || newRecipe.title.length > 100){
+          return res.status(400).json({message: "title of recipe must be between 3 and 100"})
       } else {
-          next()
+          next();
       }
   }
 
 
-const checkRecipeId = (req, res, next) => {
-    console.log("in the checkRecipeId");
 
-  }
+  const checkRecipeId = async (req, res, next) => {
+      console.log("in the checkRecipeId");
+      try{
+           const recipeId = req.params.id;
+             const recipe = await recipe.getById(recipeId);
+             if(!aRecipe){
+               res.status(404).json({message: "recipe not found"});
+             }else{
+             req.recipe = recipe;
+               next();
+           }
+    
+         }catch(error){
+           res.status(500).json({message: 'Error'})
+         }
+      
+    }
 
-
-const checkTitleNameUnique = (req, res, next) => {
-    console.log("in the checkTitleNameUnique")
-}
+///NEED TO COME BACK TO THIS ONE
+// const checkTitleNameUnique = (req, res, next) => {
+//     console.log("in the checkTitleNameUnique")
+//     const recipeTitle = req.body.title;
+//         const returnObject = {
+//             title: title
+//         }
+            
+// }
 
 
 
--- data/migrations/20210726204220_first-migration.js
+++ data/migrations/20210726204220_first-migration.js
@@ -1,21 +1,51 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema
+    .createTable('user', user => {
+        user.increments()
+        user.string('username', 300)
+            .notNullable()
+            .unique()
+            .required()
+        user.string("password", 300)
+            .notNullable()
+            .unique()
+            .required()
+        user.string("email", 300)
+            .unique()
+            .notRequired()
+    })
+    .createTable('recipe', recipe => {
+        recipe.increments('recipe_id')
+        recipe.string("title", 300)
+            .required()
+            .unique()
+        recipe.string("source", 300)
+            .required()
+        recipe.string("ingredients")
+            .required()
+        recipe.string("category")
+            .notRequired()
+    })
+    .createTable('recipe_instructions', recipe_instructions => {
+        recipe_instructions.increments('id')
+        recipe_instructions.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('recipe_id')
+            .inTable('recipe')
+            .onUpdate('RESTRICT')
+            .onDelete('RESTRICT')
+    })
 };
+
+
 
 exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists('user')
+    .dropTableIfExists('recipe')
+    .dropTableIfExists('recipe_instructions')
   
 };
 
-/////EXAMPLE
-// exports.up = function (knex) {
-//     return knex.schema.createTable('users', users => {
-//       users.increments();
-//       users.string('username', 255).notNullable().unique();
-//       users.string('password', 255).notNullable();
-//     });
-//   };
-  
-//   exports.down = function (knex) {
-//     return knex.schema.dropTableIfExists('users');
-//   };
